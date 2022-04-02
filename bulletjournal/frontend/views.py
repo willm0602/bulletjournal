585ac7324696bf16cb3ac1b907699296f17999b8
Katie Pell <katiepell@rhwlb-88-72.sonoma.edu>
2022-04-02 12:50:49 -0700

    katie's changes

--- bulletjournal/frontend/views.py
+++ bulletjournal/frontend/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 def home(req: HttpRequest):
     return render(req, "home.html")
+
+# def todo_list(request, task_id):
+#     task = get_object_or_404(Task, pk=task_id)
+
+## View examples from poll project
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+# # Create your views here.
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
