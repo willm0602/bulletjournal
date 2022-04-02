921da2f7e0e6ce4bfb746445991a27070d1c308c
willm62 <will.migdol@gmail.com>
2022-04-02 15:10:21 -0700

    migration merging

--- bulletjournal/api/models.py
+++ bulletjournal/api/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
 class Habit(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length = 255)
+    
 class Task(models.Model): # This is a subclass of the model.Model class
     # Define fields
     task_text = models.CharField(max_length = 200)
