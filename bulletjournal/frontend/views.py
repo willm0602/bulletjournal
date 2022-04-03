35326dc5fbf5e3c95c2bbbe73093a967ef0f8476
Katie Pell <katiepell@rhwlb-88-72.sonoma.edu>
2022-04-02 21:57:48 -0700

    continuing work on todo list

--- bulletjournal/frontend/views.py
+++ bulletjournal/frontend/views.py
@@ -1,3 +1,4 @@
+from asyncio import tasks
 from os import access
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
@@ -12,10 +13,9 @@ def home(req: HttpRequest):
     })
 
 def todo_add(req: HttpRequest):
-    done_tasks = Task.objects.filter(completed=True)
-    undone_tasks = Task.objects.filter(completed=False)
+    tasks = Task.objects.all()
     # print(tasks)
-    context = {"undone_tasks": undone_tasks, "done_tasks": done_tasks}
+    context = {"tasks":tasks}
     return render(req, "todolist.html", context)
 
 def get_cal(req: HttpRequest):
@@ -28,18 +28,6 @@ def get_cal(req: HttpRequest):
 # def todo_list(request, task_id):
 #     task = get_object_or_404(Task, pk=task_id)
 
-## View examples from poll project
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
-
-# # Create your views here.
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
 
 def signup(req: HttpRequest):
     return render(req, "signup.html")
